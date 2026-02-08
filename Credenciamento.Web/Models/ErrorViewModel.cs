1682dd69afc4adf172b11f0673329d7ce4d38474
RODRIGO MIRANDA <rodrigo@netrunner.com.br>
2026-02-08 12:37:34 -0300

    first commit

---
+++ Credenciamento.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Credenciamento.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
